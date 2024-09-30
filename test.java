c4bf84a2bdee330170731d9277807ba226f3a97d
TriTacLe <tritac.le@gmail.com>
2024-09-30 11:09:20 +0200

    new change

--- test.java
+++ test.java
@@ -1,6 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.err.println("hello world");
+        
+        System.err.println("hello world");
         System.err.println("hello world");
 
     }
