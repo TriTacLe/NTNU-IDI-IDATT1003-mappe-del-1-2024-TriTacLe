d091c4ec232aa9ef92f370e6cebfe860b688664d
TriTacLe <tritac.le@gmail.com>
2024-10-04 16:50:43 +0200

    endring

--- test.java
+++ test.java
@@ -1,9 +1,15 @@
+import java.util.Arrays;
+
 public class test {
     public static void main(String[] args) {
-        System.err.println("hello world");
-        
-        System.err.println("hello world");
+        int [] lst = {1,2,3,4,5};
+        int [] newlst = new int[lst.length];
+
+        for (int i = 0; i <= lst.length-1; i++) {
+            newlst[i] = lst[i];
+        }
+        System.out.println(Arrays.toString(newlst));
 
     }
-}
+}   
 
